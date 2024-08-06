746c54962e5dd2e24ce97950ab7e7b485cd00b95
SaharMFR <sweetysahora@gmail.com>
2024-08-06 11:36:36 -0700

    Fixing `lint` in task-0

--- utils/redis.js
+++ utils/redis.js
@@ -2,36 +2,34 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-	this.isConnected = true;
-        this.client.on('error', (err) => {
-	    this.isConnected = false;
-            console.error('Redis client error:', err);
-        });
+  constructor() {
+    this.client = redis.createClient();
+    this.isConnected = true;
+    this.client.on('error', (err) => {
+      this.isConnected = false;
+      console.error('Redis client error:', err);
+    });
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    }
+  isAlive() {
+    return this.isConnected;
+  }
 
-    isAlive() {
-        return this.isConnected;
-    }
+  async get(key) {
+    return this.getAsync(key);
+  }
 
-    async get(key) {
-        return await this.getAsync(key);
-    }
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    async set(key, value, duration) {
-        await this.setAsync(key, value, 'EX', duration);
-    }
-
-    async del(key) {
-        await this.delAsync(key);
-    }
-
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
